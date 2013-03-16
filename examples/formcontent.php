10afd515a18f883b0687846c3091f00073d6c04e
Marvin Kerkhoff <kerkhoff@novanet-solutions.ch>
2013-03-16 17:37:15 +0100

    First release

---
+++ examples/formcontent.php
@@ -0,0 +1,34 @@
+<div style="width:500px; padding:10px; height:200px; color:#FFF; background:#F90">
+Form Content
+
+<dl>
+	<dt>Title:</dt>
+    <dd><?=$_REQUEST['title']?></dd>
+    <dt>First Name:</dt>
+    <dd><?=$_REQUEST['firstname']?></dd>
+    <dt>Button:</dt>
+    <dd><?=$_REQUEST['firstbutton'] ? "firstbutton" : ""?><?=$_REQUEST['secondbutton'] ? "secondbutton" : ""?></dd>
+    <dt>Newsletter:</dt>
+    <dd><?=$_REQUEST['newsletter']?></dd>
+    <dt>Interest:</dt>
+    <dd>
+    <?php
+	if(!empty($_REQUEST['interest'])) {
+		foreach($_REQUEST['interest'] as $interest) {
+			echo $interest.",";
+		}
+	}
+	?>
+    </dd>
+    <dt>Filename:</dt>
+    <dd><?=$_FILES['file']['name']?></dd>
+</dl>
+</div>
+
+<style>
+dd,dt {
+	width:50%;
+	float:left;
+	margin:0px;
+}	
+</style>
